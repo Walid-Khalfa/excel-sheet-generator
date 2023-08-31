65a174a64ace39a2345e08e2ac531345f72f7a20
Walid Khalfa <khelfawalid@gmail.com>
2023-08-31 11:33:35 +0100

    Implement excel sheet generator

--- script.js
+++ script.js
@@ -1,29 +1,58 @@
-let table = document.getElementsByClassName("sheet-body")[0],
-rows = document.getElementsByClassName("rows")[0],
-columns = document.getElementsByClassName("columns")[0]
-tableExists = false
+document.addEventListener("DOMContentLoaded", function () {
+  const generateButton = document.querySelector("button[data-action='generate']");
+  const exportButton = document.querySelector("button[data-action='export']");
 
-const generateTable = () => {
-    let rowsNumber = parseInt(rows.value), columnsNumber = parseInt(columns.value)
-    table.innerHTML = ""
-    for(let i=0; i<rowsNumber; i++){
-        var tableRow = ""
-        for(let j=0; j<columnsNumber; j++){
-            tableRow += `<td contenteditable></td>`
-        }
-        table.innerHTML += tableRow
+  generateButton.addEventListener("click", function () {
+    const rowsInput = document.querySelector(".rows");
+    const columnsInput = document.querySelector(".columns");
+    const rows = parseInt(rowsInput.value);
+    const columns = parseInt(columnsInput.value);
+
+    if (isNaN(rows) || isNaN(columns) || rows <= 0 || columns <= 0) {
+      Swal.fire("Error", "Please enter valid numbers for rows and columns.", "error");
+    } else {
+      generateTable(rows, columns);
     }
-    if(rowsNumber>0 && columnsNumber>0){
-        tableExists = true
+  });
+
+  exportButton.addEventListener("click", function () {
+    const table = document.querySelector(".table table");
+
+    if (!table || table.rows.length <= 0) {
+      Swal.fire("Error", "No table to export.", "error");
+    } else {
+      ExportToExcel("xlsx");
     }
+  });
+});
+
+function generateTable(rows, columns) {
+  const sheetBody = document.querySelector(".sheet-body");
+  sheetBody.innerHTML = "";
+
+  for (let i = 0; i < rows; i++) {
+    const row = document.createElement("tr");
+
+    for (let j = 0; j < columns; j++) {
+      const cell = document.createElement("td");
+      cell.textContent = `Row ${i + 1}, Col ${j + 1}`;
+      row.appendChild(cell);
+    }
+
+    sheetBody.appendChild(row);
+  }
 }
 
-const ExportToExcel = (type, fn, dl) => {
-    if(!tableExists){
-        return
-    }
-    var elt = table
-    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" })
-    return dl ? XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' })
-        : XLSX.writeFile(wb, fn || ('MyNewSheet.' + (type || 'xlsx')))
-}
+const ExportToExcel = (fileType) => {
+  const table = document.querySelector(".table table");
+
+  if (!table || table.rows.length <= 0 || table.rows[0].cells.length <= 0) {
+    Swal.fire("Error", "No table to export.", "error");
+    return;
+  }
+
+  const wb = XLSX.utils.table_to_book(table, { sheet: "SheetJS" });
+  const wbout = XLSX.write(wb, { bookType: fileType, bookSST: true, type: "array" });
+
+  saveAs(new Blob([wbout], { type: "application/octet-stream" }), "sheet." + fileType);
+};
